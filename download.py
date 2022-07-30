7f7bddaa07d1092aad34dba4a2366452a45a2cfc
Julian Espinel <julianespinel@gmail.com>
2022-07-30 16:46:48 -0400

    Add logic to download stock historical data

---
+++ download.py
@@ -0,0 +1,24 @@
+import numpy as np
+import yfinance as yf
+from pandas.core.frame import DataFrame
+
+from common_types import Period
+
+
+def get_stock_historical_data(symbol: str, period: Period) -> DataFrame:
+    '''
+    Returns historical data in descending order.
+    :param symbol:
+    :param period:
+    :return:
+    '''
+    ticker = yf.Ticker(symbol)
+    prices = ticker.history(period=period)
+    prices.sort_index(ascending=False, inplace=True)
+    prices['change'] = (prices.Close - prices.Close.shift(-1)) / prices.Close.shift(-1)
+    prices['log_return'] = (np.log(prices.Close / prices.Close.shift(-1)))
+    return prices
+
+
+prices = get_stock_historical_data('AMZN', Period.YEAR)
+print(prices)
