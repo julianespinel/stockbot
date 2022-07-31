3d19840107a71bd06d8fceba46a286f2aa65989e
Julian Espinel <julianespinel@gmail.com>
2022-07-31 15:27:59 -0400

    Improve get_stock_historical_data documentation

--- download.py
+++ download.py
@@ -6,12 +6,12 @@ from common_types import Period
 
 
 def get_stock_historical_data(symbol: str, period: Period) -> DataFrame:
-    '''
+    """
     Returns historical data in descending order.
-    :param symbol:
-    :param period:
-    :return:
-    '''
+    :param symbol: Symbol of the stock we want to get historical data.
+    :param period: Time range of the historical data we want to get.
+    :return: A pandas dataframe with the historical data in descending order.
+    """
     ticker = yf.Ticker(symbol)
     prices = ticker.history(period=period)
     prices.sort_index(ascending=False, inplace=True)
