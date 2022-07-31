3d07664e0448d839d9afbefcb6219ebc048f0027
Julian Espinel <julianespinel@gmail.com>
2022-07-31 15:54:29 -0400

    Add methods to a class to make them easier to test

--- download.py
+++ download.py
@@ -1,24 +1,24 @@
 import numpy as np
-import yfinance as yf
 from pandas.core.frame import DataFrame
 
 from common_types import Period
 
 
-def get_stock_historical_data(symbol: str, period: Period) -> DataFrame:
-    """
-    Returns historical data in descending order.
-    :param symbol: Symbol of the stock we want to get historical data.
-    :param period: Time range of the historical data we want to get.
-    :return: A pandas dataframe with the historical data in descending order.
-    """
-    ticker = yf.Ticker(symbol)
-    prices = ticker.history(period=period)
-    prices.sort_index(ascending=False, inplace=True)
-    prices['change'] = (prices.Close - prices.Close.shift(-1)) / prices.Close.shift(-1)
-    prices['log_return'] = (np.log(prices.Close / prices.Close.shift(-1)))
-    return prices
+class Download:
 
+    def __init__(self, financelib):
+        self.financelib = financelib
 
-prices = get_stock_historical_data('AMZN', Period.YEAR)
-print(prices)
+    def get_stock_historical_data(self, symbol: str, period: Period) -> DataFrame:
+        """
+        Returns historical data in descending order.
+        :param symbol: Symbol of the stock we want to get historical data.
+        :param period: Time range of the historical data we want to get.
+        :return: A pandas dataframe with the historical data in descending order.
+        """
+        ticker = self.financelib.Ticker(symbol)
+        prices = ticker.history(period=period)
+        prices.sort_index(ascending=False, inplace=True)
+        prices['change'] = (prices.Close - prices.Close.shift(-1)) / prices.Close.shift(-1)
+        prices['log_return'] = (np.log(prices.Close / prices.Close.shift(-1)))
+        return prices
