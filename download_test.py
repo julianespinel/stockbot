a2161ff40c6c736f53f3dc2d1e922c0653387761
Julian Espinel <julianespinel@gmail.com>
2022-08-01 17:15:43 -0400

    Define yfinance mock in a method

--- download_test.py
+++ download_test.py
@@ -12,20 +12,25 @@ class DownloadTests(unittest.TestCase):
 
     def test_get_stock_historical_data_returns_valid_data(self):
         # arrange
-        expected_df = pd.read_csv('csv_files/AMZN.csv')
-        mocked_ticker = MagicMock()
-        mocked_ticker.history = MagicMock(return_value=expected_df)
-        yf.Ticker = MagicMock(return_value=mocked_ticker)
+        mocked_yf = self._get_mocked_yfinance()
 
         symbol = 'AMZN'
         period = Period.YEAR
-        downloader = Download(yf)
+        downloader = Download(mocked_yf)
         # act
         prices = downloader.get_stock_historical_data(symbol, period)
         # assert
         self.assertEqual(len(prices), 252)
         self.assertEqual(len(prices.change), 252)
         self.assertEqual(len(prices.log_return), 252)
+
+    @staticmethod
+    def _get_mocked_yfinance():
+        expected_df = pd.read_csv('csv_files/AMZN_from_yfinance.csv')
+        mocked_ticker = MagicMock()
+        mocked_ticker.history = MagicMock(return_value=expected_df)
+        yf.Ticker = MagicMock(return_value=mocked_ticker)
+        return yf
 
 
 if __name__ == '__main__':
