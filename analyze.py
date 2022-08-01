1e807467b1d5481b9447a58157ab37cc88201f57
Julian Espinel <julianespinel@gmail.com>
2022-08-01 17:17:24 -0400

    Define return, price and volatility stats

--- analyze.py
+++ analyze.py
@@ -1,43 +1,126 @@
 import math
-from datetime import datetime
+from datetime import date
 from typing import NamedTuple
 
 from pandas.core.frame import DataFrame
 
 from common_types import Period
 
-
-class PriceStats(NamedTuple):
-    min_close: float
-    max_close: float
-    difference: float
-    max_negative_change: float
-    max_positive_change: float
-
-    # def __str__(self):
-    #     attributes = [self.min_close, self.max_close, self.difference,
-    #                   self.max_negative_change, self.max_positive_change]
-    #     strings = [str(attribute) for attribute in attributes]
-    #     return ','.join(strings)
+DECIMAL_PLACES = 4
 
 
-def get_low_and_high_prices(prices: DataFrame) -> None:
-    print(f'1 m: {_get_price_stats_in_period(prices, Period.MONTH)}')
-    print(f'3 m: {_get_price_stats_in_period(prices, Period.QUARTER)}')
-    print(f'6 m: {_get_price_stats_in_period(prices, Period.HALF)}')
-    print(f'12m: {_get_price_stats_in_period(prices, Period.YEAR)}')
+class ReturnStats(NamedTuple):
+    month: float
+    quarter: float
+    half: float
+    year: float
+
+    def round(self):
+        return ReturnStats(
+            round(self.month, DECIMAL_PLACES),
+            round(self.quarter, DECIMAL_PLACES),
+            round(self.half, DECIMAL_PLACES),
+            round(self.year, DECIMAL_PLACES),
+        )
 
 
-def get_volatility(prices: DataFrame) -> None:
-    print(f'1 m: {_get_volatility_in_period(prices, Period.MONTH)}')
-    print(f'3 m: {_get_volatility_in_period(prices, Period.QUARTER)}')
-    print(f'6 m: {_get_volatility_in_period(prices, Period.HALF)}')
-    print(f'12m: {_get_volatility_in_period(prices, Period.YEAR)}')
+class ClosePrice:
+
+    def __init__(self, str_date: str, close_value: float):
+        self.date = date.fromisoformat(str_date)
+        self.value = close_value
+
+    def round(self):
+        return ClosePrice(
+            str(self.date),
+            round(self.value, DECIMAL_PLACES),
+        )
+
+    def __str__(self):
+        return f'{self.date}, {self.value}'
+
+    def __eq__(self, other):
+        if not isinstance(other, ClosePrice):
+            return False
+        return self.date == other.date and self.value == other.value
+
+
+class PriceStats:
+
+    def __init__(self, min_price: ClosePrice, max_price: ClosePrice,
+                 close_price_difference: float,
+                 max_negative_change: float, max_positive_change: float):
+        self.min_price = min_price
+        self.max_price = max_price
+        self.close_price_difference = close_price_difference
+        self.max_negative_change = max_negative_change
+        self.max_positive_change = max_positive_change
+
+    def round(self):
+        return PriceStats(
+            self.min_price.round(),
+            self.max_price.round(),
+            round(self.close_price_difference, DECIMAL_PLACES),
+            round(self.max_negative_change, DECIMAL_PLACES),
+            round(self.max_positive_change, DECIMAL_PLACES),
+        )
+
+    def __str__(self):
+        attributes = [self.min_price, self.max_price,
+                      self.close_price_difference,
+                      self.max_negative_change, self.max_positive_change]
+        strings = [str(attribute) for attribute in attributes]
+        return ','.join(strings)
+
+    def __eq__(self, other):
+        if not isinstance(other, PriceStats):
+            return False
+
+        return (self.min_price == other.min_price
+                and self.max_price == other.max_price
+                and self.close_price_difference == other.close_price_difference
+                and self.max_negative_change == other.max_negative_change
+                and self.max_positive_change == other.max_positive_change)
+
+
+def get_return_stats(prices: DataFrame) -> ReturnStats:
+    return ReturnStats(
+        _get_return_in_period(prices, Period.MONTH),
+        _get_return_in_period(prices, Period.QUARTER),
+        _get_return_in_period(prices, Period.HALF),
+        _get_return_in_period(prices, Period.YEAR),
+    )
+
+
+def get_price_stats(prices: DataFrame) -> dict[Period, PriceStats]:
+    return {
+        Period.MONTH: _get_price_stats_in_period(prices, Period.MONTH),
+        Period.QUARTER: _get_price_stats_in_period(prices, Period.QUARTER),
+        Period.HALF: _get_price_stats_in_period(prices, Period.HALF),
+        Period.YEAR: _get_price_stats_in_period(prices, Period.YEAR),
+    }
+
+
+def get_volatility(prices: DataFrame) -> dict[Period, float]:
+    return {
+        Period.MONTH: _get_volatility_in_period(prices, Period.MONTH),
+        Period.QUARTER: _get_volatility_in_period(prices, Period.QUARTER),
+        Period.HALF: _get_volatility_in_period(prices, Period.HALF),
+        Period.YEAR: _get_volatility_in_period(prices, Period.YEAR),
+    }
 
 
 # -----------------------------------------------------------------------------
 # private methods
 # -----------------------------------------------------------------------------
+def _get_return_in_period(prices: DataFrame, period: Period):
+    today_row = prices.iloc[0]
+    today_price = ClosePrice(today_row.Date, today_row.Close)
+
+    initial_row = prices.iloc[_get_trading_days(period) - 1]
+    initial_price = ClosePrice(initial_row.Date, initial_row.Close)
+    return _get_price_difference(today_price, initial_price)
+
 
 def _get_price_stats_in_period(prices: DataFrame, period: Period) -> PriceStats:
     """
@@ -46,36 +129,53 @@ def _get_price_stats_in_period(prices: DataFrame, period: Period) -> PriceStats:
     :param period: Period to get the statistics from the prices dataframe.
     :return: A PriceStats object containing the required information.
     """
+    min_price = _get_min_price(prices, period)
+    max_price = _get_max_price(prices, period)
+
+    difference = _get_price_difference(min_price, max_price)
+
     trading_days = _get_trading_days(period)
-    min_close_date = prices.Close[:trading_days].idxmin()
-    min_close = prices.loc[min_close_date].Close
-    max_close_date = prices.Close[:trading_days].idxmax()
-    max_close = prices.loc[max_close_date].Close
-
-    difference = _get_price_difference(min_close_date, max_close_date, min_close, max_close)
-
     max_negative_change = prices.change[:trading_days].min()
     max_positive_change = prices.change[:trading_days].max()
 
-    return PriceStats(min_close, max_close, difference, max_negative_change, max_positive_change)
+    return PriceStats(min_price, max_price, difference,
+                      max_negative_change, max_positive_change)
 
 
-def _get_price_difference(min_close_date: datetime, max_close_date: datetime, min_close: float,
-                          max_close: float) -> float:
-    initial, final = (min_close, max_close) if min_close_date <= max_close_date else (max_close, min_close)
-    difference = final / initial - 1
-    return difference
+def _get_price_difference(a_price: ClosePrice, other_price: ClosePrice) -> float:
+    if a_price.date <= other_price.date:
+        initial, final = a_price, other_price
+    else:
+        initial, final = other_price, a_price
+
+    return final.value / initial.value - 1
+
+
+def _get_min_price(prices: DataFrame, period: Period) -> ClosePrice:
+    trading_days = _get_trading_days(period)
+    row_id = prices.Close[:trading_days].idxmin()
+    row = prices.iloc[row_id]
+    return ClosePrice(row.Date, row.Close)
+
+
+def _get_max_price(prices: DataFrame, period: Period) -> ClosePrice:
+    trading_days = _get_trading_days(period)
+    row_id = prices.Close[:trading_days].idxmax()
+    row = prices.iloc[row_id]
+    return ClosePrice(row.Date, row.Close)
 
 
 def _get_volatility_in_period(prices: DataFrame, period: Period) -> float:
     """
     Return volatility defined as sigma(p) = daily sigma * sqrt(p),
     where p = trading days in the given period.
+    See: https://en.wikipedia.org/wiki/Volatility_(finance)#Mathematical_definition
     """
     # calculate daily logarithmic return
     trading_days = _get_trading_days(period)
     daily_std = prices.log_return[:trading_days].std()
-    return daily_std * math.sqrt(trading_days)
+    volatility = daily_std * math.sqrt(trading_days)
+    return round(volatility, DECIMAL_PLACES)
 
 
 def _get_trading_days(period: Period) -> int:
@@ -89,10 +189,3 @@ def _get_trading_days(period: Period) -> int:
         case _:
             # Return year by default
             return 252
-
-
-import download
-
-prices = download.get_stock_historical_data('AMZN', Period.YEAR)
-get_low_and_high_prices(prices)
-get_volatility(prices)
