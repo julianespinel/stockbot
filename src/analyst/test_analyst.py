08f5a5f9fb4ea8890026552163826e694fac9a2c
Julian Espinel <julianespinel@gmail.com>
2022-09-17 20:20:43 -0400

    Add logic to detect a basic price anomaly

--- src/analyst/test_analyst.py
+++ src/analyst/test_analyst.py
@@ -1,9 +1,10 @@
 import unittest
+from unittest.mock import patch
 
 import pandas as pd
 
 from analyst import analyst
-from common.types import PriceStats, ClosePrice
+from common.types import PriceStats, ClosePrice, PriceAnomaly, Period
 
 
 class AnalystTests(unittest.TestCase):
@@ -64,6 +65,46 @@ class AnalystTests(unittest.TestCase):
         self.assertEqual(volatility.half, 0.3995)
         self.assertEqual(volatility.year, 0.4350)
 
+    @patch('analyst.analyst.get_current_price')
+    def test_get_price_anomaly_return_year_anomaly(self, get_current_price):
+        # arrange
+        current_price = ClosePrice('2022-07-29', 1.0)
+        # expected_price is less than the min price of the last year
+        get_current_price.return_value = current_price
+        prices = pd.read_csv('analyst/test_files/AMZN_from_stockbot.csv')
+        # act
+        anomaly = analyst.get_price_anomaly(prices)
+        # assert
+        self.assertIsNotNone(anomaly)
+        self.assertEqual(
+            anomaly.round(), self._get_expected_year_price_alert(current_price))
+
+    @patch('analyst.analyst.get_current_price')
+    def test_get_price_anomaly_return_month_anomaly(self, get_current_price):
+        # arrange
+        current_price = ClosePrice('2022-07-29', 104.0)
+        # expected_price is less than the min price of the last month
+        get_current_price.return_value = current_price
+        prices = pd.read_csv('analyst/test_files/AMZN_from_stockbot.csv')
+        # act
+        anomaly = analyst.get_price_anomaly(prices)
+        # assert
+        self.assertIsNotNone(anomaly)
+        self.assertEqual(
+            anomaly.round(), self._get_expected_month_price_alert(current_price))
+
+    @patch('analyst.analyst.get_current_price')
+    def test_get_price_anomaly_return_none(self, get_current_price):
+        # arrange
+        expected_price = ClosePrice('2022-07-29', 120.0)
+        # expected_price is within min and max of the last year
+        get_current_price.return_value = expected_price
+        prices = pd.read_csv('analyst/test_files/AMZN_from_stockbot.csv')
+        # act
+        anomaly = analyst.get_price_anomaly(prices)
+        # assert
+        self.assertIsNone(anomaly)
+
     @staticmethod
     def _get_expected_month():
         min_close = ClosePrice('2022-06-30', 106.21)
@@ -91,3 +132,15 @@ class AnalystTests(unittest.TestCase):
         max_close = ClosePrice('2021-11-18', 184.8030)
         expected_month = PriceStats(min_close, max_close, -0.4464, -0.1405, 0.1354)
         return expected_month
+
+    @staticmethod
+    def _get_expected_month_price_alert(current_price: ClosePrice):
+        min_price = ClosePrice('2022-06-30', 106.21)
+        max_price = ClosePrice('2022-07-29', 134.95)
+        return PriceAnomaly(Period.MONTH, min_price, current_price, max_price)
+
+    @staticmethod
+    def _get_expected_year_price_alert(current_price: ClosePrice):
+        min_price = ClosePrice('2022-06-14', 102.31)
+        max_price = ClosePrice('2021-11-18', 184.8030)
+        return PriceAnomaly(Period.YEAR, min_price, current_price, max_price)
